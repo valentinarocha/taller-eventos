6ebe7d530bc81110cdf97c0e65dfc235cbace7b0
Valentina <valyazrochizq@hotmail.com>
2024-08-13 13:36:57 -0300

    cambio solucion-click

--- eventos.js
+++ eventos.js
@@ -1,10 +1,14 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-   
     const div = document.querySelector('.button-container');
-    
+    const button = document.getElementById('Button');
+
   
     div.addEventListener('click', function() {
         alert('¡Hola! Soy el div');
+    });
+
+    button.addEventListener('click', function(event) {
+        event.stopPropagation(); 
+        alert('Has apretado el botón!');
     });
 });
