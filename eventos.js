3e1bfa4033ffc84149c193214e3c716d306ca7a2
Valentina <valyazrochizq@hotmail.com>
2024-08-13 13:14:32 -0300

    cambio con div

---
+++ eventos.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+   
+    const div = document.querySelector('.button-container');
+    
+  
+    div.addEventListener('click', function() {
+        alert('¡Hola! Soy el div');
+    });
+});
